608fa1fa7e2f3ef0196172435e53edb545f18aad
sebi90 <sebwie90@googlemail.com>
2015-06-21 17:06:13 +0200

    aufgabe 1 d und e hinzugefügt

--- app/src/main/java/sebi/persistenzapplikations/Main2Activity.java
+++ app/src/main/java/sebi/persistenzapplikations/Main2Activity.java
@@ -1,61 +1,38 @@
 package sebi.persistenzapplikations;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-public class MainActivity extends Activity {
+public class Main2Activity extends Activity {
 
     private static final String PREFS_NAME = "MyPrefsFile";
     private static final String PREFS_VALUE = "MyStringValue";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main2, menu);
         return true;
     }
 
-    public void savePreferences(View view)
+    public void getSharedPreferences(View view)
     {
-        EditText editText = (EditText) findViewById(R.id.editText);
+        TextView textView = (TextView) findViewById(R.id.textView2);
         // aufgabe e = MODE_MULTI_PROCESS setzen
         SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_MULTI_PROCESS);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString(PREFS_VALUE, editText.getText().toString());
-
-        // Commit the edits!
-        editor.commit();
-    }
-
-    public void getPreferences(View view)
-    {
-        TextView textView = (TextView) findViewById(R.id.textView);
-        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
         textView.setText("");
         textView.setText(prefs.getString(PREFS_VALUE,"No Val"));
-    }
-
-    //Aufgabe d
-    public void startActivity(View view)
-    {
-        Intent intent = new Intent();
-        intent.setClass(this, Main2Activity.class);
-        startActivity(intent);
     }
 
     @Override
@@ -71,18 +48,5 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("MyMainActivity" , "onStop");
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("MyMainActivity" , "onPause");
     }
 }
